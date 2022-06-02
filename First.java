e8fb37212004568920c4e613c00bed30b6a5534d
7dragonwarrior7 <91316776+7dragonwarrior7@users.noreply.github.com>
2022-06-03 03:04:09 +0530

    Create First.java

---
+++ First.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+class First
+{
+	public static void main(String args[])
+	{
+		String name;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("May I Know your Name");
+		name=sc.nextLine();
+		System.out.println("Welcome " +name);
+	}
+}
